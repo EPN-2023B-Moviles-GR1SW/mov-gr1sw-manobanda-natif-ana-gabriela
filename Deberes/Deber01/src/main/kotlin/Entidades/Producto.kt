2c343813c5b3f83d68d0c00854f999af51407a13
mngabriela <anagabriela.manobanda@epn.edu.ec>
2024-01-04 02:25:40 -0500

    Añadir Clases

--- Deberes/Deber01/src/main/kotlin/Entidades/Producto.kt
+++ Deberes/Deber01/src/main/kotlin/Entidades/Producto.kt
@@ -1,16 +1,22 @@
 package Entidades
 
-
-import java.util.*
+import Core.AccesoDatosProducto
 
 class Producto(
-    var id: Int,
+    var codigo: Int,
     var nombre: String,
     var cantidadDisponible: Int,
-    var tienda:Tienda,
+    var tienda: Tienda,
     var disponible: Boolean,
     var precioUnitario: Double
 ) {
+
+    private val accesoDatosProducto =
+        AccesoDatosProducto(System.getProperty("user.dir") + "\\src\\main\\kotlin\\archivos\\Productos.txt")
+
+    override fun toString(): String {
+        return "Producto(id=$codigo, nombre='$nombre', cantidadDisponible=$cantidadDisponible, tienda=$tienda, " + "disponible=$disponible, precioUnitario=$precioUnitario, accesoDatosProducto=$accesoDatosProducto)"
+    }
 
 
 }
--- Deberes/Deber01/src/main/kotlin/Entidades/Tienda.kt
+++ Deberes/Deber01/src/main/kotlin/Entidades/Tienda.kt
@@ -1,14 +1,33 @@
 package Entidades
 
 
+import Core.AccesoDatosTienda
+import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
 
-class Tienda (
-    val id: Int,
+class Tienda(
+    val id: Int = idCounter.getAndIncrement(),
     val nombre: String,
     val fechaApertura: Date,
     val direccion: String,
-    val numeroEmpleados: Int
-){
+    val numeroEmpleados: Int,
+    val contacto: String
+) {
+    companion object {
+        private val idCounter = AtomicInteger(0)
+    }
+
+    private val accesoDatosTienda =
+        AccesoDatosTienda(System.getProperty("user.dir") + "\\src\\main\\kotlin\\archivos\\Tienda.txt")
+
+    constructor(id: Int, nombre: String, fechaApertura: String, direccion: String, numeroEmpleados: Int, contacto: String) :
+            this(id, nombre, SimpleDateFormat("dd/MM/yyyy").parse(fechaApertura), direccion, numeroEmpleados, contacto)
+
+
+    override fun toString(): String {
+        return "Tienda(id=$id, nombre='$nombre', fechaApertura=$fechaApertura, direccion='$direccion', numeroEmpleados=$numeroEmpleados, contacto='$contacto', accesoDatosTienda=$accesoDatosTienda)"
+    }
+
 
 }
